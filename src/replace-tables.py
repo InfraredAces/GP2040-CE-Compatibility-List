ac65586e44430ce9bf61fe32b7900b5d2cd0f422
Kevin Nguyen <nguyenkevin625@gmail.com>
2023-08-23 21:34:29 -0700

    Update file names for import

--- src/replace-tables.py
+++ src/replace-tables.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-ps5_compatibility = pd.read_csv('playstation-5-Compatibility.csv', keep_default_na=False)
-usb_passthrough_devices = pd.read_csv('usb-passthrough-devices.csv', keep_default_na=False)
+ps5_compatibility = pd.read_csv('GP2040-CE Compatibility List - PS4 Games on PS5.csv', keep_default_na=False)
+usb_passthrough_devices = pd.read_csv('GP2040-CE Compatibility List - USB Passthrough.csv', keep_default_na=False)
 
 with open('README.md', 'r') as readme:
     data = readme.read()
